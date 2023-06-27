3e014c76dc7362f3931aa57e99adc7ac7d9763b7
y <kpreston@drw.com>
2023-06-27 16:58:23 -0500

    Cleaning up implementation

--- loadTest.js
+++ loadTest.js
@@ -1,10 +1,5 @@
 console.log('Start load test');
 
-let numRequestsComplete = 0;
-let requestDurationSum = 0;
-let lastHundredDuration = new Array();
-let lastHundredDurationSum = 0;
-
 function createRequest() {
   return new Promise((resolve) => {
     const xhr = new XMLHttpRequest();
@@ -22,39 +17,52 @@ function createRequest() {
   })
 }
 
-async function createRequestLoop() {
-  console.log('Initializing request loop');
-  while(true) {
-    const start = Date.now();
-    await createRequest();
-    const end = Date.now();
-    const duration = end - start;
-    lastHundredDuration.push(duration);
-    lastHundredDurationSum += duration;
-    if(lastHundredDuration.length > 100) {
-      const head = lastHundredDuration.shift();
-      lastHundredDurationSum -= head;
-    }
-    requestDurationSum += duration;
-    numRequestsComplete++;
-    const totalNode = document.querySelector('.total-requests');
-    if(totalNode) {
-      totalNode.textContent = numRequestsComplete;
-    }
-    const durationNode = document.querySelector('.duration');
-    if(duration) {
-      const avg = requestDurationSum / numRequestsComplete;
-      durationNode.textContent = avg;
-    }
-    const lastHundredNode = document.querySelector('.last-hundred');
-    if(lastHundredNode) {
-      const avg = lastHundredDurationSum / lastHundredDuration.length;
-      lastHundredNode.textContent = avg;
+window.addEventListener('load', function(event){
+  // Load DOM elements fpr displaying data
+  const userAgentNode = document.querySelector('.user-agent');
+  const totalNumRequestsNode = document.querySelector('.total-requests');
+  const durationNode = document.querySelector('.duration');
+  const lastHundredNode = document.querySelector('.last-hundred');
+
+  // Initialize stats
+  let numRequestsComplete = 0;
+  let totalDurationSum = 0;
+  let lastHundredDuration = new Array();
+  let lastHundredDurationSum = 0;
+
+  // Function that continuously polls the rest endpoint
+  async function createRequestLoop() {
+    console.log('Initializing request loop');
+    while(true) {
+      const start = Date.now();
+      await createRequest();
+      const end = Date.now();
+      const duration = end - start;
+      lastHundredDuration.push(duration);
+      lastHundredDurationSum += duration;
+      if(lastHundredDuration.length > 100) {
+        const head = lastHundredDuration.shift();
+        lastHundredDurationSum -= head;
+      }
+      totalDurationSum += duration;
+      numRequestsComplete++;
+      
+      totalNumRequestsNode.textContent = numRequestsComplete;
+      
+      const totalAvg = totalDurationSum / numRequestsComplete;
+      durationNode.textContent = totalAvg;
+      
+      const rollingAvg = lastHundredDurationSum / lastHundredDuration.length;
+      lastHundredNode.textContent = rollingAvg;
     }
   }
-}
 
-// Create 10 request loops
-for(let i = 0; i < 10; i++) {
-  createRequestLoop();
-}
+  // Create 10 request loops
+  for(let i = 0; i < 10; i++) {
+    createRequestLoop();
+  }
+
+  // Show user agent on page
+  const chromeVersion = window.navigator.userAgent.match(/Chrome\/[\d\.]+/)[0]
+  userAgentNode.textContent = chromeVersion;
+});
