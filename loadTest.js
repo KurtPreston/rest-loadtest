aad3b789ae17d71b7d59c6a3f4bb1ca8ccabb47b
y <kpreston@drw.com>
2023-06-27 17:08:51 -0500

    Show time running

--- loadTest.js
+++ loadTest.js
@@ -23,12 +23,15 @@ window.addEventListener('load', function(event){
   const totalNumRequestsNode = document.querySelector('.total-requests');
   const durationNode = document.querySelector('.duration');
   const lastHundredNode = document.querySelector('.last-hundred');
+  const timeRunningNode = document.querySelector('.time-running');
 
   // Initialize stats
   let numRequestsComplete = 0;
   let totalDurationSum = 0;
   let lastHundredDuration = new Array();
   let lastHundredDurationSum = 0;
+
+  const testStartTime = Date.now();
 
   // Function that continuously polls the rest endpoint
   async function createRequestLoop() {
@@ -46,14 +49,6 @@ window.addEventListener('load', function(event){
       }
       totalDurationSum += duration;
       numRequestsComplete++;
-      
-      totalNumRequestsNode.textContent = numRequestsComplete;
-      
-      const totalAvg = totalDurationSum / numRequestsComplete;
-      durationNode.textContent = totalAvg;
-      
-      const rollingAvg = lastHundredDurationSum / lastHundredDuration.length;
-      lastHundredNode.textContent = rollingAvg;
     }
   }
 
@@ -61,6 +56,22 @@ window.addEventListener('load', function(event){
   for(let i = 0; i < 10; i++) {
     createRequestLoop();
   }
+
+  // Update display
+  setInterval(() => {
+    totalNumRequestsNode.textContent = numRequestsComplete;
+      
+    const totalAvg = totalDurationSum / numRequestsComplete;
+    durationNode.textContent = totalAvg;
+    
+    const rollingAvg = lastHundredDurationSum / lastHundredDuration.length;
+    lastHundredNode.textContent = rollingAvg;
+
+    const timeRunning = Date.now() - testStartTime;
+    const timeRunningMinutes = Math.floor(timeRunning / 60000);
+    const timeRunningSeconds = Math.floor((timeRunning - timeRunningMinutes * 60000) / 1000);
+    timeRunningNode.textContent = `${timeRunningMinutes}:${timeRunningSeconds.toString().padStart(2, '0')}`
+  }, 50);
 
   // Show user agent on page
   const chromeVersion = window.navigator.userAgent.match(/Chrome\/[\d\.]+/)[0]
